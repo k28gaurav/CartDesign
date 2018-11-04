b18c890cec2297acc8f4c72de74a77098f3704e7
kumargaurav <kumar.g@toppr.com>
2018-11-04 12:24:32 +0530

    Added usecase to fetch all items

--- app/src/main/java/com/gaurav/cartsystem/domain/interactor/FetchItemListUseCase.kt
+++ app/src/main/java/com/gaurav/cartsystem/domain/interactor/FetchItemListUseCase.kt
@@ -1,4 +1,12 @@
 package com.gaurav.cartsystem.domain.interactor
 
-class FetchItemListUseCase {
+import com.gaurav.cartsystem.data.db.entities.Item
+import com.gaurav.cartsystem.domain.repository.CartRepository
+import io.reactivex.Single
+import javax.inject.Inject
+
+class FetchItemListUseCase @Inject constructor(private val cartRepository: CartRepository) {
+    fun execute(): Single<List<Item>> {
+        return cartRepository.fetchItems()
+    }
 }
