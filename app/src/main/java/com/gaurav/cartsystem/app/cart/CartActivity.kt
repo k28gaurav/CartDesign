b890e6385527d9696b0cb39ab1c2f8ee3a0ba0dc
kumargaurav <kumar.g@toppr.com>
2018-11-04 12:16:43 +0530

    Added fun for fragment transaction

--- app/src/main/java/com/gaurav/cartsystem/app/cart/CartActivity.kt
+++ app/src/main/java/com/gaurav/cartsystem/app/cart/CartActivity.kt
@@ -5,9 +5,14 @@ import android.arch.lifecycle.ViewModelProviders
 import android.os.Bundle
 import com.gaurav.cartsystem.R
 import com.gaurav.cartsystem.app.base.DaggerBaseActivity
+import com.gaurav.cartsystem.data.db.entities.Item
+import kotlinx.android.synthetic.main.activity_cart.fl_cart_container
+import kotlinx.android.synthetic.main.activity_cart.fl_library_container
 import javax.inject.Inject
 
-class CartActivity : DaggerBaseActivity<CartViewModel>(), LibraryFragment.ChangeFragmentListener {
+class CartActivity : DaggerBaseActivity<CartViewModel>(), LibraryFragment.ChangeFragmentListener,
+        AllItemsFragment.ShowAddItemToCartDialogListener{
+
 
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
 
@@ -22,7 +27,26 @@ class CartActivity : DaggerBaseActivity<CartViewModel>(), LibraryFragment.Change
         initViews()
     }
 
-    override fun onFragmentChange(fragmentName: LibraryFragment.NextFrag) {
+    override fun initViews() {
+        supportFragmentManager?.beginTransaction()
+                ?.add(fl_library_container.id, libraryFragment, "library")
+                ?.add(fl_cart_container.id, cartFragment, "cart")
+                ?.commit()
+        super.initViews()
+    }
 
+    override fun onFragmentChange(fragmentName: LibraryFragment.NextFrag) {
+        when(fragmentName) {
+            LibraryFragment.NextFrag.ALL_ITEMS -> {
+                supportFragmentManager?.beginTransaction()
+                        ?.replace(fl_library_container.id, allItemsFragment, "allItems")
+                        ?.commit()
+            }
+            else -> {}
+        }
+    }
+
+    override fun onShowAddItemToCartDialog(item: Item) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
