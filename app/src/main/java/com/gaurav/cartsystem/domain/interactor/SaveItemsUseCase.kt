ae1c6cbff4c4d7e22a9ecbba6ccb1b23bf249658
kumargaurav <kumar.g@toppr.com>
2018-11-04 12:23:43 +0530

    Use Case to save all items

--- app/src/main/java/com/gaurav/cartsystem/domain/interactor/SaveItemsUseCase.kt
+++ app/src/main/java/com/gaurav/cartsystem/domain/interactor/SaveItemsUseCase.kt
@@ -1,4 +1,11 @@
 package com.gaurav.cartsystem.domain.interactor
 
-class SaveItemUseCase {
+import com.gaurav.cartsystem.data.db.entities.Item
+import com.gaurav.cartsystem.domain.repository.CartRepository
+import javax.inject.Inject
+
+class SaveItemsUseCase @Inject constructor(private val cartRepository: CartRepository) {
+    fun execute(items: List<Item>) {
+        cartRepository.saveAllItems(items)
+    }
 }
