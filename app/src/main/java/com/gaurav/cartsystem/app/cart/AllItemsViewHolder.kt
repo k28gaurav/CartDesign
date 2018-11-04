76a20d8d377a57fdbf56da1a9607e946afafca3b
kumargaurav <kumar.g@toppr.com>
2018-11-04 11:54:30 +0530

    Added AllItemViewHolder

--- app/src/main/java/com/gaurav/cartsystem/app/cart/AllItemsViewHolder.kt
+++ app/src/main/java/com/gaurav/cartsystem/app/cart/AllItemsViewHolder.kt
@@ -1,6 +1,19 @@
 package com.gaurav.cartsystem.app.cart
 
+import android.view.View
 import com.gaurav.cartsystem.app.base.BaseViewHolder
+import com.gaurav.cartsystem.data.db.entities.Item
+import kotlinx.android.synthetic.main.item_cart.view.tv_item_name
 
-class AllItemsViewHolder: BaseViewHolder<> {
+class AllItemsViewHolder(val view: View, val onClick : (position:Int) -> Unit): BaseViewHolder<Item>(view) {
+
+    init {
+        view.setOnClickListener {
+            onClick.invoke(adapterPosition)
+        }
+    }
+
+    override fun setData(data: Item) {
+        view.tv_item_name.text = data.title
+    }
 }
